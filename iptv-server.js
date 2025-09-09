b545985e0e8e9097c61f802bf8c80a77f3d31e58
Thang Nguyen <quaywin@gmail.com>
2025-09-09 21:31:22 +0700

    Change server port to 4444 and add package.json

--- iptv-server.js
+++ iptv-server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 
-const PORT = 3000;
+const PORT = 4444;
 const M3U_FILE_PATH = path.join(__dirname, "current_playlist.m3u");
 
 // Route serve file M3U
