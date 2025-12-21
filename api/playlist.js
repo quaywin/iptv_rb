c1444679e367b42b10523288e6e45e4cc2c50421
Thang Nguyen <quaywin@gmail.com>
2025-12-21 19:45:49 +0700

    chore: Update playlist proxy VPS_URL IP address.

--- api/playlist.js
+++ api/playlist.js
@@ -1,6 +1,6 @@
 // Vercel Serverless Function acting as a Proxy to your VPS
 module.exports = async (req, res) => {
-  const VPS_URL = "http://217.15.164.89:4444/playlist.m3u";
+  const VPS_URL = "http://222.255.214.22:4444/playlist.m3u";
 
   try {
     const response = await fetch(VPS_URL);
